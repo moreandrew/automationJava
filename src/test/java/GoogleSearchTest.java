12b95e6d2377330b8ebee70bcd5a7f3966cb56f7
moreandrew <more.andrew@yandex.ru>
2021-12-01 22:20:38 -0500

    lecture 11 17 code

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -6,18 +6,43 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 import java.time.Duration;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class GoogleSearchTest {
+
+    WebDriver driver;
+
+
+    @BeforeSuite
+    public void suiteSetup(){
+        String os = System.getProperty("os.name");
+
+        if (os.contains("Win")){
+            System.setProperty("webdriver.chrome.driver", "C:/Webdriver/bin/chromedriver.exe");
+        }
+    }
+
     @Test
-    public void testName() {
-        System.setProperty("webdriver.chrome.driver", "C:/Webdriver/bin/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+    public void testOS() {
+
+        //System.out.println(os);
+    }
+
+    @Parameters({"browser"})
+    @Test
+    public void testGoogleSearch(String browser) {
+
+        if (browser.equalsIgnoreCase("chrome"))  driver = new ChromeDriver();
+
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         driver.get("https://google.com/");
 
         WebElement textInput = driver.findElement(By.name("q"));
         textInput.sendKeys("Portnov computer school");
+        textInput.submit();
     }
 }
